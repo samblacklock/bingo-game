6727d5f54e962524ff1a958379e0be0ac2e8e22d
samblacklock <samblacklock@me.com>
2017-01-27 17:29:01 +0000

    now vue app

---
+++ js/app/component.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>HELLO U</h1>
+    <game v-for="game of games" :gameString="game"></game>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style lang="sass" src="./style.scss" scoped></style>
---
+++ js/game/component.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :data-nums="gameString">
+    <h2>{{gameString}}</h2>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style lang="sass" src="./style.scss" scoped></style>
