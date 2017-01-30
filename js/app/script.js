f7b7182ed55f94ce9c5fd2ae709f39eb09c69c2c
samblacklock <samblacklock@me.com>
2017-01-30 23:54:20 +0000

    update

--- js/app/script.js
+++ js/app/script.js
@@ -7,7 +7,72 @@ export default {
   },
   data() {
     return {
-      games: this.$parent.games
+      games: this.parseTicketString(this.$parent.games)
     }
+  },
+  methods: {
+    parseTicketString(ticketString) {
+      const schema = { 0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' };
+      const gameArray = [];
+
+      const gameRows = ticketString.match(/.{1,10}/g).map((row) => {
+        return row.match(/.{1,2}/g)
+      });
+
+      for(let row of gameRows) {
+        const gameObj = JSON.parse(JSON.stringify(schema));
+
+        row.forEach((num) => {
+          const index = num.charAt(0);
+          gameObj[index] = parseInt(num, 10);
+
+          if(gameObj[9]) {
+            gameObj[8] = gameObj[9];
+          }
+
+          delete gameObj[9];
+        });
+
+        gameArray.push(gameObj);
+      };
+
+      let sortedGames = [];
+
+      while(gameArray.length) {
+        sortedGames.push(gameArray.splice(0,3));
+      }
+
+      return sortedGames;
+    },
+
+    generateLookupTable(games) {
+      const lookupTable = {};
+      games.forEach((game) => {
+        game.forEach((b) => {
+          for(let c in b) {
+            (function(c, b) {
+              let index = Object.keys(lookupTable).length + 1;
+              lookupTable[index] = null;
+              Object.defineProperty(lookupTable, index, {
+                get: () => { return b[c] },
+                set: (val) => { b[c] = val; return val }
+              });
+            })(c, b);
+          }
+        })
+      });
+
+      return lookupTable;
+    },
+
+    playBingo(numbers) {
+      console.log(numbers);
+    }
+  },
+  created() {
+    const games = this.$data.games;
+    this.table = this.generateLookupTable(games);
+    console.log(this.table);
+    this.playBingo(games);
   }
 }
--- js/main.js
+++ js/main.js
@@ -2,63 +2,13 @@ import Vue from 'vue';
 import App from './app/component.vue';
 
 const tickets = "011722475204365360702637497481233455758302154058881928446789061241507324334876840738576186051132437816395663800818206590104559628214294664710935667287132130687703253151692742547985";
-const games = parseTicketString(tickets);
 
 const vm = new Vue({
   el: '#app',
   data: () => {
     return {
-      games: games
+      games: tickets
     }
   },
   render: createElement => createElement(App),
 });
-
-lookupTable(games);
-
-
-function parseTicketString(ticketString) {
-  const schema = { 0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' };
-  const gameArray = [];
-
-  const gameRows = ticketString.match(/.{1,10}/g).map((row) => {
-    return row.match(/.{1,2}/g)
-  });
-
-  for(let row of gameRows) {
-    const gameObj = JSON.parse(JSON.stringify(schema));
-
-    row.forEach((num) => {
-      const index = num.charAt(0);
-      gameObj[index] = parseInt(num, 10);
-
-      if(gameObj[9]) {
-        gameObj[8] = gameObj[9];
-      }
-
-      delete gameObj[9];
-    });
-
-    gameArray.push(gameObj);
-  };
-
-  let sortedGames = [];
-
-  while(gameArray.length) {
-    sortedGames.push(gameArray.splice(0,3));
-  }
-
-  return sortedGames;
-}
-
-function lookupTable(games) {
-  window.lookupTable = {};
-
-  games.forEach((a, i) => {
-    a.forEach((b) => {
-      for(let c in b) {
-        window.lookupTable[Object.keys(window.lookupTable).length + 1] = a[b[c]]
-      }
-    })
-  });
-}
